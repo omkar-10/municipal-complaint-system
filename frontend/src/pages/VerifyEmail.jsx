191829497eb9e242f95837657bae54d8f39831f8
Omkar Dhavale <dhavale.omkar04@gmail.com>
2025-07-06 13:59:20 +0530

    Added email verification flow

--- frontend/src/pages/VerifyEmail.jsx
+++ frontend/src/pages/VerifyEmail.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router";
 import api from "../utils/axiosInstance";
 import { Loader2, CheckCircle2, XCircle } from "lucide-react";
 
